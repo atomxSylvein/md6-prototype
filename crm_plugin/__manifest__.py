e4fd4b01a06c215aa6c78f81e93189a63c3d9b38
atomxSylvein <sylvein.faraux@atomxsystem.com>
2019-01-23 15:35:49 +0100

    Ajout du plugin de crm

---
+++ crm_plugin/__manifest__.py
@@ -0,0 +1,26 @@
+# -*- coding: utf-8 -*-
+{
+    'name': "CRM Plugin",
+
+    'summary': """Add fields in CRM module""",
+
+    'description': """This module adds some fields to opportunities (CRM module)""",
+
+    'author': "AtomX System",
+
+    'website': "http://atomxsystem.eu",
+
+    'category': 'Tools',
+
+    'version': '0.1',
+
+    # any module necessary for this one to work correctly
+    'depends': ['contact_plugin', 'crm'],
+
+    # always loaded
+    'data': [
+        #'views/form_view.xml',
+    ],
+    'installable': True,
+    'auto_install': False,
+}
