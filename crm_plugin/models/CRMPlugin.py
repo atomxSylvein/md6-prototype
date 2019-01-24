55160f59cfd49934b0d5498ec5c192d3b8f66eac
atomxSylvein <sylvein.faraux@atomxsystem.com>
2019-01-24 15:26:15 +0100

    correction

--- crm_plugin/models/CRMPlugin.py
+++ crm_plugin/models/CRMPlugin.py
@@ -25,6 +25,6 @@ class CRMPlugin(models.Model):
 	m_buy = fields.Monetary(string="Prix d'achat", currency_field='company_currency')
 	m_sell = fields.Monetary(string="Prix de vente", currency_field='company_currency')
 	m_margin = fields.Monetary(string="Marge", currency_field='company_currency', readonly=True)
-	m_TJM = fields.Monetary(string="TJM", currency_field='company_currency')
+	m_tjm = fields.Monetary(string="TJM", currency_field='company_currency')
 	m_package_price = fields.Monetary(string="Prix forfait", currency_field='company_currency')
 	
