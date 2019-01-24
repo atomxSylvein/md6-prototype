4f26cf01cee974b93218d47854f5d34002871389
atomxSylvein <sylvein.faraux@atomxsystem.com>
2019-01-24 10:53:44 +0100

    modification du modèle crm.lead ajout du plugin aux tags de l'opportunité ajout de la vue liste et form des tags

--- crm_plugin/models/CRMPlugin.py
+++ crm_plugin/models/CRMPlugin.py
@@ -7,18 +7,22 @@ class CRMPlugin(models.Model):
 	"""This class extends CRM class and provides implements the projects statuses
 	
 	Attributes:
-	    m_prospect_type (selection): A contact can be a suspect, prospect or customer
-	    m_SI (many2one): Informative System of the company
 	"""
 	
 	_inherit = 'crm.lead'
-	#m_type = fields.Selection([('distribution','Distribution'), ('project','Projet')], default='distribution', string="Type d'opportunité")
-	tags_id = fields.Many2one('crm.lead.tag', string="Type de business")
+	m_type = fields.Selection([('distribution','Distribution'), ('project','Projet')], default='distribution', string="Type de business")
+	tags_id = fields.Many2many('crm.lead.tag', string="Actions")
 	partner_id = fields.Many2one(domain="[('company_type', '=', 'company')]")
 	m_contact = fields.Many2one('res.partner', domain="[('parent_id', '=', 'partner_id')]", string="Contact de la société")
+	m_lead_provider = fields.Many2one('res.partner', string="Fournisseur du lead")
 	m_maturity = fields.Selection([('month', 'Dans le mois'), ('quarter', 'Dans le trimestre'), ('ahead', 'En amon de phase')], default='month', string="Maturité de l'opportunité")
 	date_deadline = fields.Date(string="Date de fin de l'opportunité")
 	m_company_city = fields.Char(related='partner_id.city', string="Ville", store=False, readonly=True)
 	m_company_zip = fields.Char(related='partner_id.zip', string="Département", store=False, readonly=True)
 	m_estimated_revenue = fields.Monetary(string="Budget estimé", currency_field='company_currency')
-	m_lead_provider = fields.Many2one('res.partner', string="Fournisseur du lead")
+	m_buy = fields.Monetary(string="Prix d'achat", currency_field='company_currency')
+	m_sell = fields.Monetary(string="Prix de vente", currency_field='company_currency')
+	m_margin = fields.Monetary(string="Marge", readonly=True)
+	m_TJM = fields.Monetary(string="TJM", currency_field='company_currency')
+	m_package_price = fields.Monetary(string="Prix forfait", currency_field='company_currency')
+	
--- crm_plugin/models/__init__.py
+++ crm_plugin/models/__init__.py
@@ -1,3 +1,4 @@
 # -*- coding: utf-8 -*-
 
+from . import CRMTagPlugin
 from . import CRMPlugin
