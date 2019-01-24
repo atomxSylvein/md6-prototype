8132dee4389cb50bdcdced926a13fe1fd9074495
atomxSylvein <sylvein.faraux@atomxsystem.com>
2019-01-24 18:06:31 +0100

    correction

--- contact_plugin/models/__init__.py
+++ contact_plugin/models/__init__.py
@@ -1,4 +1,3 @@
 # -*- coding: utf-8 -*-
 
-from . import informationSystem
 from . import contactPlugin
--- contact_plugin/models/contactPlugin.py
+++ contact_plugin/models/contactPlugin.py
@@ -1,7 +1,6 @@
 # -*- coding: utf-8 -*-
 
 from odoo import models, fields, api
-from . import informationSystem
 
 class ContactPlugin(models.Model):
 
