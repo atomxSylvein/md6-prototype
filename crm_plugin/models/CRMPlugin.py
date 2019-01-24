2c9d5c3eccebc488a9ac837e61bda00ce11f0370
atomxSylvein <sylvein.faraux@atomxsystem.com>
2019-01-24 16:28:34 +0100

    ajout des champs relatifs dans la vue form

--- crm_plugin/models/CRMPlugin.py
+++ crm_plugin/models/CRMPlugin.py
@@ -10,16 +10,22 @@ class CRMPlugin(models.Model):
 	"""
 	
 	_inherit = 'crm.lead'
-	m_type = fields.Selection([('distribution','Distribution'), ('project','Projet')], default='distribution', string="Type d'opportunité")
+	
+	#modification of existing fields
 	tags_id = fields.Many2many('crm.lead.tag', string="Type de business")
 	partner_id = fields.Many2one(domain="[('company_type', '=', 'company')]")
-	m_stage_probability = fields.Float(related='stage_id.probability', string="Probabilité", readonly=True, store=False)
+	email_from = fields.Char(related='m_contact.email', string="Email")
+	date_deadline = fields.Date(string="Date de fin de l'opportunité")
+	partner_address_phone = fields.Char(related='m_contact.phone', string="Numéro de tél.")
+
+	#new fields
+	m_type = fields.Selection([('distribution','Distribution'), ('project','Projet')], default='distribution', string="Type d'opportunité")
 	m_company = fields.Many2one(related="partner_id")
 	m_company_name = fields.Char(related="partner_id.name")
 	m_contact = fields.Many2one('res.partner', string="Contact de la société")
 	m_lead_provider = fields.Many2one('res.partner', string="Fournisseur du lead")
 	m_maturity = fields.Selection([('month', 'Dans le mois'), ('quarter', 'Dans le trimestre'), ('ahead', 'En amon de phase')], default='month', string="Maturité de l'opportunité")
-	date_deadline = fields.Date(string="Date de fin de l'opportunité")
+	m_stage_probability = fields.Float(related='stage_id.probability', string="Probabilité", readonly=True, store=False)
 	m_company_city = fields.Char(related='partner_id.city', string="Ville", store=False, readonly=True)
 	m_company_zip = fields.Char(related='partner_id.zip', string="Département", store=False, readonly=True)
 	m_estimated_revenue = fields.Monetary(string="Budget estimé", currency_field='company_currency')
